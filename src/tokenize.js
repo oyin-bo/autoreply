528c5916fa9fc37704226326923ec72e07417e07
Oleg Mihailik <mihailik@gmail.com>
2025-09-27 15:24:38 +0100

    Dummy tokenize file.

---
+++ src/tokenize.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @typedef {number} RawToken
+ */
+
+/**
+ * Tokenizes text into reasonable tokens using SentencePiece logic.
+ * @param {string} text
+ * @param {RawToken[]} tokens
+ */
+export function tokenize(text, tokens) {
+  //
+}
